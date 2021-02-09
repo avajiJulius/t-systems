6f672ef193e66b3550473999916aad4288e19b92
Alexandr Matushkin <avajijulius@icloud.com>
2021-02-09 20:20:46 +0300

    refactor: add order and truck info tables

--- logiweb-info/src/main/java/com/logiweb/avaji/model/Order.java
+++ logiweb-info/src/main/java/com/logiweb/avaji/model/Order.java
@@ -1,53 +1,23 @@
 package com.logiweb.avaji.model;
 
-
-import java.util.Set;
-
 public class Order {
 
-    private long id;
-    private boolean status;
-    private String truckId;
-    private Set<Driver> drivers;
-    private double remainingTime;
+    private long orderId;
+    private boolean completed;
 
-    public long getId() {
-        return id;
+    public long getOrderId() {
+        return orderId;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setOrderId(long orderId) {
+        this.orderId = orderId;
     }
 
-    public boolean isStatus() {
-        return status;
+    public boolean isCompleted() {
+        return completed;
     }
 
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public String getTruckId() {
-        return truckId;
-    }
-
-    public void setTruckId(String truckId) {
-        this.truckId = truckId;
-    }
-
-    public Set<Driver> getDrivers() {
-        return drivers;
-    }
-
-    public void setDrivers(Set<Driver> drivers) {
-        this.drivers = drivers;
-    }
-
-    public double getRemainingTime() {
-        return remainingTime;
-    }
-
-    public void setRemainingTime(double remainingTime) {
-        this.remainingTime = remainingTime;
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
