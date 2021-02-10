c7478acd43563f19b888e5dcc565e057332b3fed
Alexandr Matushkin <avajijulius@icloud.com>
2021-02-10 09:08:30 +0300

    refactor: bug fix
    Fix bug when view update have delay. The bug occurred due to the fact
    that InformationService been @Statlles and don't have @ApplicationView.
    Because of that InformationService have proxy and this proxy give delay
    information for update.

--- logiweb-info/src/main/java/com/logiweb/avaji/service/InformationService.java
+++ logiweb-info/src/main/java/com/logiweb/avaji/service/InformationService.java
@@ -4,12 +4,14 @@ import com.logiweb.avaji.model.Information;
 import com.logiweb.avaji.util.InformationUpdateEvent;
 
 import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
+import javax.ejb.Stateful;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 
-@Stateless
+@Stateful
 @LocalBean
+@ApplicationScoped
 public class InformationService {
 
     private Information information;
